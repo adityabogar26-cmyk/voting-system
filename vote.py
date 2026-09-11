cea59217b3e98941c7b17e7bc4d66ee185508bd3
adityabogar26-cmyk <adityabogar26@gmail.com>
2026-09-11 14:36:36 +0530

    Fix spelling errors in voting eligibility messages
    Corrected spelling errors in eligibility messages.

--- vote.py
+++ vote.py
@@ -2,5 +2,7 @@ age = int(input("enter your age:"))
 
 if age >=18:
   print("your are eligibale to vote.")
+  print("you can register asa voter.")
 else:
   print("your are not eligible to vote.")
+  print("you must at least 18 years old.")
